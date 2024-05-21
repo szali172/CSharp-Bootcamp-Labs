a3b91172b92394febef774b1a07a8500b7368acd
Zuhair Ali <szali172@gmail.com>
2024-05-20 20:05:02 -0500

    Basic String Processing complete

---
+++ BasicStringProcessing/BasicStringProcessing/Program.cs
@@ -0,0 +1,21 @@
+﻿
+while (true)
+{
+
+    Console.Write("Enter a sentence: << ");
+    string sentence = Console.ReadLine();
+
+
+    foreach (string word in sentence.Split(' '))
+    {
+        Console.WriteLine(word);
+    }
+
+    Console.WriteLine("Would you like to continue? (y/n)");
+    string yesOrNo = Console.ReadLine();
+
+    if (yesOrNo == "n")
+    {
+        break;
+    }
+}
