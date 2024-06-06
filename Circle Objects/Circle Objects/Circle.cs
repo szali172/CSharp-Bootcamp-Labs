7efd4ad0c845183eb349ad85b212c0d4c711049e
Zuhair Ali <szali172@gmail.com>
2024-06-05 20:09:31 -0500

    Circle Objects Lab complete

---
+++ Circle Objects/Circle Objects/Circle.cs
@@ -0,0 +1,37 @@
+﻿
+namespace Circle_Objects;
+
+public class Circle
+{
+    private double radius;
+
+    public Circle(double radius)
+    {
+        this.radius = radius;
+    }
+
+    public double CalculateDiameter()
+    {
+        return radius * 2.0;
+    }
+    public double CalculateCircumference()
+    {
+        return 2.0 * Math.PI * radius;
+    }
+
+    public double CalculateArea()
+    {
+        return radius * radius * Math.PI;
+    }
+
+    public void Grow()
+    {
+        radius = radius * 2.0;
+    }
+
+    public double GetRadius()
+    {
+        return radius;
+    }
+
+}
---
+++ Circle Objects/Circle Objects/Program.cs
@@ -0,0 +1,51 @@
+﻿using Circle_Objects;
+
+Console.WriteLine("Welcome to the Circle Tester");
+
+while (true)
+{  
+    Console.Write("Enter a radius: ");
+
+    try
+    {
+        double radius = double.Parse(Console.ReadLine());
+        Circle circle = new Circle(radius);
+        Console.WriteLine($"Diameter: {circle.CalculateDiameter()}");
+        Console.WriteLine($"Circumference: {circle.CalculateCircumference()}");
+        Console.WriteLine($"Area: {circle.CalculateArea()}");
+
+        while (true)
+        {
+            Console.WriteLine("Should the circle grow? (y/n)");
+
+            try
+            {
+                string input = Console.ReadLine();
+
+                if(input == "y")
+                {
+                    circle.Grow();
+                    Console.WriteLine($"Diameter: {circle.CalculateDiameter()}");
+                    Console.WriteLine($"Circumference: {circle.CalculateCircumference()}");
+                    Console.WriteLine($"Area: {circle.CalculateArea()}");
+                }
+                else
+                {
+                    Console.WriteLine("Goodbye. The circle's final radius is " + circle.GetRadius());
+                    return;
+                }
+
+            } catch
+            {
+                Console.WriteLine("Please enter either \"y\" or \"n\"");
+            }
+
+        }
+        
+
+    } catch
+    {
+        Console.WriteLine("Please pass a valid number for the radius");
+    }
+    
+}
